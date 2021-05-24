9951d57e66c0930b6697238f029158f8e13d3c93
wxbt <50980203+wxbt@users.noreply.github.com>
2021-05-24 16:30:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 /**
  * Four Digit Display
  */
-//% weight=100 color=#191970 icon="\uf145" block="B_四位数码管"
+//% weight=100 color=#1874CD icon="\uf145" block="B_四位数码管"
 namespace TM1637 {
     let TM1637_CMD1 = 0x40;
     let TM1637_CMD2 = 0xC0;
